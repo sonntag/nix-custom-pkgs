60e711fc1b02c2e5420bb48b226f5061d396c0ed
Justin Sonntag <justin.sonntag@gmail.com>
2026-03-26 21:01:51 -0700

    Add overlays

--- modules/default.nix
+++ modules/default.nix
@@ -1,14 +1,18 @@
-{
+{...}: let
+  packages = pkgs: {
+    # cljstyle = pkgs.callPackage ./_pkgs/cljstyle.nix {};
+    defaultbrowser = pkgs.callPackage ./_pkgs/defaultbrowser.nix {};
+    desktoppr = pkgs.callPackage ./_pkgs/desktoppr.nix {};
+    # karabiner-driverkit = pkgs.callPackage ./_pkgs/karabiner-driverkit.nix {};
+    mdts = pkgs.callPackage ./_pkgs/mdts.nix {};
+    sidecar = pkgs.callPackage ./_pkgs/sidecar.nix {};
+    slack-mcp-server = pkgs.callPackage ./_pkgs/slack-mcp-server.nix {};
+    td = pkgs.callPackage ./_pkgs/td.nix {};
+  };
+in {
+  flake.overlays.default = final: _prev: packages final;
+
   perSystem = {pkgs, ...}: {
-    packages = {
-      # cljstyle = pkgs.callPackage ./_pkgs/cljstyle.nix {};
-      defaultbrowser = pkgs.callPackage ./_pkgs/defaultbrowser.nix {};
-      desktoppr = pkgs.callPackage ./_pkgs/desktoppr.nix {};
-      # karabiner-driverkit = pkgs.callPackage ./_pkgs/karabiner-driverkit.nix {};
-      mdts = pkgs.callPackage ./_pkgs/mdts.nix {};
-      sidecar = pkgs.callPackage ./_pkgs/sidecar.nix {};
-      slack-mcp-server = pkgs.callPackage ./_pkgs/slack-mcp-server.nix {};
-      td = pkgs.callPackage ./_pkgs/td.nix {};
-    };
+    packages = packages pkgs;
   };
 }
