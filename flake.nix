bfcb8e1dc5fd267cce09b2d0b8f496a7603b8137
Justin Sonntag <justin.sonntag@gmail.com>
2026-04-10 01:00:56 -0700

    Refactor flake

--- flake.nix
+++ flake.nix
@@ -3,12 +3,12 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    import-tree.url = "github:vic/import-tree";
+    pkgs-by-name.url = "github:drupol/pkgs-by-name-for-flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} {
     systems = import inputs.systems;
-    imports = [(inputs.import-tree ./modules)];
+    imports = [./modules];
   };
 }
--- modules/default.nix
+++ modules/default.nix
@@ -1,19 +1,8 @@
-{...}: let
-  packages = pkgs: {
-    # cljstyle = pkgs.callPackage ./_pkgs/cljstyle.nix {};
-    codebase-memory-mcp = pkgs.callPackage ./_pkgs/codebase-memory-mcp.nix {};
-    defaultbrowser = pkgs.callPackage ./_pkgs/defaultbrowser.nix {};
-    desktoppr = pkgs.callPackage ./_pkgs/desktoppr.nix {};
-    # karabiner-driverkit = pkgs.callPackage ./_pkgs/karabiner-driverkit.nix {};
-    mdts = pkgs.callPackage ./_pkgs/mdts.nix {};
-    sidecar = pkgs.callPackage ./_pkgs/sidecar.nix {};
-    slack-mcp-server = pkgs.callPackage ./_pkgs/slack-mcp-server.nix {};
-    td = pkgs.callPackage ./_pkgs/td.nix {};
-  };
-in {
-  flake.overlays.default = final: _prev: packages final;
+{inputs, withSystem, ...}: {
+  imports = [inputs.pkgs-by-name.flakeModule];
 
-  perSystem = {pkgs, ...}: {
-    packages = packages pkgs;
-  };
+  perSystem.pkgsDirectory = ../packages;
+
+  flake.overlays.default = final: _prev:
+    withSystem final.stdenv.hostPlatform.system ({packages, ...}: packages);
 }
